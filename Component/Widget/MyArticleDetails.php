1d500fdede4bb4d2a9f1c1f5f602c746de59466c
mkrepp <m.krepp@gmx.de>
2021-02-07 14:12:45 +0100

    added MyArticleDetails.php

---
+++ Component/Widget/MyArticleDetails.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace mkrepp\mytestmodule\Component\Widget;
+
+class MyArticleDetails extends MyArticleDetails_parent
+{
+    public function render()
+    {
+        return parent::render();
+    }
+}
