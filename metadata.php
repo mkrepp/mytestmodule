184020c88a5dcd4d7be767d81f0d42673e981523
mkrepp <m.krepp@gmx.de>
2021-02-07 14:16:54 +0100

    updated metadata.php

--- metadata.php
+++ metadata.php
@@ -22,7 +22,8 @@ $aModule = array(
     'url'         => '',
     'email'       => 'hepppes@gmx.de',
     'extend'      => array(
-        \OxidEsales\Eshop\Application\Controller\ArticleDetailsController::class => \mkrepp\mytestmodule\Controller\MyArticleDetailsController::class
+        \OxidEsales\Eshop\Application\Controller\ArticleDetailsController::class => \mkrepp\mytestmodule\Controller\MyArticleDetailsController::class,
+        \OxidEsales\Eshop\Application\Component\Widget\ArticleDetails::class => \mkrepp\mytestmodule\Component\Widget\MyArticleDetails::class
     ),
     'controllers'       => array(
         'mytestcontroller' => \mkrepp\mytestmodule\Controller\MyTestController::class,
